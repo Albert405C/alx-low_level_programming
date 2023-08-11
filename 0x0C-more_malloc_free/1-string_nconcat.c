637f403f6debe07140dfcf2ede7a514922f66d0f
AlbertMitto <94157652+Albert405C@users.noreply.github.com>
2023-08-11 11:31:07 +0300

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,12 +14,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s;
 	unsigned int a = 0, b = 0, len1 = 0, len2 = 0;
 
-	/* Treat NULL strings as empty strings */
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
 	/* Calculate lengths of s1 and s2 */
 	while (s1 && s1[len1])
 		len1++;
@@ -44,13 +38,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	/* Copy n bytes from s2 to concatenated string */
 	while (n < len2 && a < (len1 + n))
-	{
+	
 		s[a++] = s2[b++];
-	}
+	
 	while (n >= len2 && a < (len1 + len2))
-	{
+	
 		s[a++] = s2[b++];
-	}
+	
 
 	/* Null-terminate the concatenated string */
 	s[a] = '\0';
