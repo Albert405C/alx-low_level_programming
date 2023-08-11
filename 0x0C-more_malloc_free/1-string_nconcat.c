7afbba4f81a6119b20072ffd2f313c0a86a2453c
AlbertMitto <94157652+Albert405C@users.noreply.github.com>
2023-08-11 12:04:05 +0300

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,39 +10,40 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *s;
-    unsigned int a = 0, b = 0, c = 0;
+        char *s;
+        unsigned int a = 0, b = 0, c = 0;
 
-    if (s1 == NULL)
-    {
-        a = 0;
-    }
-    else
-    {
-        while (s1[a])
-            a++;
-    }
-    if (s2 == NULL)
-    {
-        b = 0;
-    }
-    else
-    {
-        while (s2[b])
-            b++;
-    }
-    if (b > n)
-        b = n;
+        if (s1 == NULL)
+        {
+                a = 0;
+        }
+        else
+        {
+                while (s1[a])
+                        a++;
+        }
+        if (s2 == NULL)
+        {
+                b = 0;
+        }
+        else
+        {
+                while (s2[b])
+                        b++;
+        }
+        if (b > n)
+                b = n;
 
-    s = malloc(sizeof(char) * (a + b + 1));
-    if (s == NULL)
-        return (NULL);
+        s = malloc(sizeof(char) * (a + b + 1));
+        if (s == NULL)
+                return (NULL);
 
-    for (c = 0; c < a; c++)
-        s[c] = s1[c];
-    for (c = 0; c < b; c++)
-        s[c + a] = s2[c];
+        for (c = 0; c < a; c++)
+                s[c] = s1[c];
+        for (c = 0; c < b; c++)
+                s[c + a] = s2[c];
 
-    s[a + b] = '\0';
-    return (s);
+        s[a + b] = '\0';
+        return (s);
 }
+
