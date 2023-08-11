ccb455d5ffaf249a30c6fa1d69e90d5d1b3e7a06
AlbertMitto <94157652+Albert405C@users.noreply.github.com>
2023-08-11 11:39:23 +0300

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,18 +9,17 @@
  *
  * Return: A pointer to the concatenated string, or NULL on failure.
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
 	unsigned int a = 0, b = 0, len1 = 0, len2 = 0;
 
-	/* Calculate lengths of s1 and s2 */
 	while (s1 && s1[len1])
 		len1++;
 	while (s2 && s2[len2])
 		len2++;
 
-	/* Allocate memory for the concatenated string */
 	if (n < len2)
 		s = malloc(sizeof(char) * (len1 + n + 1));
 	else
@@ -29,25 +28,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (!s)
 		return (NULL);
 
-	/* Copy s1 to concatenated string */
-	while (a < len1)
+	while (i < len1)
 	{
 		s[a] = s1[a];
 		a++;
 	}
 
-	/* Copy n bytes from s2 to concatenated string */
-	while (n < len2 && a < (len1 + n))
-	
+	while (n < len2 && i < (len1 + n))
 		s[a++] = s2[b++];
-	
-	while (n >= len2 && a < (len1 + len2))
-	
-		s[a++] = s2[b++];
-	
 
-	/* Null-terminate the concatenated string */
+	while (n >= len2 && i < (len1 + len2))
+		s[a++] = s2[b++];
+
 	s[a] = '\0';
 
 	return (s);
 }
+
