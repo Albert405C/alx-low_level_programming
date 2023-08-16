a721e26318893e54340c9e61e0bc1411419abb49
AlbertMitto <94157652+Albert405C@users.noreply.github.com>
2023-08-16 23:51:48 +0300

    function_pointers.h

--- 0x0F.C-Function_pointers/function_pointers.h
+++ 0x0F.C-Function_pointers/function_pointers.h
@@ -6,7 +6,7 @@ int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
-#endif
+
 
 
 #endif /* FUNCTION_POINTERS_H */
