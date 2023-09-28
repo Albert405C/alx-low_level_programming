d7ff9c69b040cf39f83f8d13068cb013f949e233
AlbertMitto <94157652+Albert405C@users.noreply.github.com>
2023-09-28 21:35:15 +0300

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,19 +8,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    int index;
-    unsigned int result = 0;
+	int index;
+	unsigned int result = 0;
 
-    if (!b)
-        return (0);
+	if (!b)
+		return (0);
 
-    for (index = 0; b[index]; index++)
-    {
-        if (b[index] < '0' || b[index] > '1')
-            return (0);
-        result = 2 * result + (b[index] - '0');
-    }
+	for (index = 0; b[index]; index++)
+	{
+		if (b[index] < '0' || b[index] > '1')
+			return (0);
+		result = 2 * result + (b[index] - '0');
+	}
 
-    return (result);
+	return (result);
 }
 
